383741bdecc5d87a339267e01d87424db9fd4ff9
Mikael Kopteff <miku.kopteff@gmail.com>
2012-11-15 10:00:57 +0200

    Add static serving of files from node js.

--- server/api.js
+++ server/api.js
@@ -1,14 +1,2 @@
 
-exports.http = function(){
-	var express = require('express');
-	var app = express();
-	var httpPort = 8080;
-
-	app.get('/hello', function(req, res){
-  		res.send('Shit gets done\n');
-	});
-
-	console.log("listening to http at " + httpPort)
-	app.listen(httpPort);
-}
 
--- server/server.js
+++ server/server.js
@@ -1,40 +1,45 @@
-var WebSocketServer = require('websocket').server;
+var sendMessageToGame = function(){console.log('empty function')};
+
+var _ = require('underscore')
+var express = require('express');
+var app = express();
+var httpPort = 8080;
+app.use('/public', express.static(_.first(__dirname, _.lastIndexOf(__dirname, '/')).join('') + '/target'));
+console.log(_.first(__dirname, _.lastIndexOf(__dirname, '/')).join('') + '/target');
+
+app.listen(httpPort);
+console.log("listening to http at " + httpPort)
+
+app.get('/shoot', function(req, res){
+    res.send('Api received a message\n');
+    console.log(sendMessageToGame);
+    sendMessageToGame();
+});
+
+var websocketPort = 1337;
+var gameSocketServer = require('websocket').server;
 var http = require('http');
-var api = require('./api.js');
-api.http()
+var server = http.createServer(function(){});
+server.listen(websocketPort, function() { console.log('listening to websocket accept ' + websocketPort) });
+gameSocketServer = new gameSocketServer({ httpServer: server });
 
-var port = 1337;
-var gameConnection = function(){};
-
-var server = http.createServer(function(request, response) {
-    console.log("http request received");
-    gameConnection();
-    response.end();
-});
-
-server.listen(port, function() { console.log("listening to websocket accept " + port) });
-
-wsServer = new WebSocketServer({
-    httpServer: server
-});
-
-
-wsServer.on('request', function(request) {
+gameSocketServer.on('request', function(request) {
     var connection = request.accept(null, request.origin);
-    gameConnection = function() {
-        var json = JSON.stringify({ type:'message', data: "movePaddle" });
+    sendMessageToGame = function() {
+        console.log('writing data out to game');
+        var json = JSON.stringify({ type:'message', data: 'shoot' });
         connection.sendUTF(json);
     }
 
     connection.on('message', function(message) {
         if (message.type === 'utf8') {
-            console.log("TADAA" + message);
+            console.log('message received' + message);
         }
     });
 
     connection.on('close', function(connection) {
-        gameConnection = function(){};
-        console.log("Connection closed");
+        sendMessageToGame = function(){};
+        console.log('Connection closed');
     });
 });
 
